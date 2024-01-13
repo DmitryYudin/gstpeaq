80551092ce7802be2c066665c28ad82b1853647c
dima <yudind@gmail.com>
2024-01-14 02:16:15 +0300

    remove unused

--- autogen.sh
+++
@@ -1,8 +0,0 @@
-git log --pretty --numstat --summary | git2cl >ChangeLog
-libtoolize
-gtkdocize
-aclocal -I m4
-autoheader
-autoconf
-automake --add-missing
-CFLAGS="-Wall -Werror -g -DGST_DISABLE_DEPRECATED" ./configure --enable-gtk-doc --enable-man --prefix=/usr $@
